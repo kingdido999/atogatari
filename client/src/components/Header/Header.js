141d95585fb4e22e4242a6fa26f4dbff198d8ec4
kingdido999 <kingdido999@gmail.com>
2017-02-24 16:50:10 -0800

    fix menu size

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
     const { isAuthenticated } = this.props
 
     return (
-      <Menu pointing secondary size="big">
+      <Menu pointing secondary size="large">
         <Menu.Item as={Link} to="/" name='bangumi pic' />
 
         <Menu.Menu position='right'>
