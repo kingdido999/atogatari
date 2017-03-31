e4cae7c7eef2b58ce2b39541b0174f9e7f5d2170
kingdido999 <kingdido999@gmail.com>
2017-03-30 20:34:36 -0700

    Refactor Screenshot

--- client/src/containers/Screenshot.js
+++ client/src/containers/Screenshot.js
@@ -30,85 +30,133 @@ class Screenshot extends Component {
   }
 
   render () {
-    const { params, dispatch, isAuthenticated, users, bangumis, screenshots, screenshotFavorites, userFavorites } = this.props
-    const { screenshotId } = params
-    const screenshot = screenshots[screenshotId]
-    if (!screenshot) return null
-
-    const bangumi = bangumis[screenshot.bangumi]
-
-    if (!bangumi) return null
-    const { _id, file, episode } = screenshot
-
-    const isFavorited = isAuthenticated &&
-      screenshotFavorites[screenshotId].ids.filter(favoriteId => {
-        return userFavorites.ids.includes(favoriteId)
-      }).length > 0
-
-    const favoritesCount = screenshotFavorites[screenshotId].ids.length
-    const uploader = users[screenshot.user]
-    if (!uploader) return null
-
     return (
       <Container>
         <Grid stackable>
           <Grid.Row columns={2}>
             <Grid.Column width={4}>
               <Segment.Group>
-                <Segment>
-                  <Header>
-                    <Link to={`/bangumi/${bangumi._id}`}>
-                      {bangumi.title}
-                    </Link>
-                  </Header>
-
-                  {bangumi.aliases.length > 0 &&
-                    <List>
-                      {bangumi.aliases.map(alias =>
-                        <List.Item>{alias}</List.Item>
-                      )}
-                    </List>
-                  }
-                </Segment>
-                <Segment>
-                  Episode {episode}
-                </Segment>
-                <Segment>
-                  Uploader: <Link to={`/user/${uploader._id}`}>{uploader.username}</Link>
-                </Segment>
-                {screenshot.tags.length > 0 &&
-                  <Segment>
-                    <Tags tags={screenshot.tags} />
-                  </Segment>
-                }
-                <Segment>
-                  <Button.Group>
-                    <FavoriteButton
-                      dispatch={dispatch}
-                      screenshotId={_id}
-                      isFavorited={isFavorited}
-                      favoritesCount={favoritesCount}
-                      isAuthenticated={isAuthenticated}
-                    />
-                    <DownloadButton
-                      file={file}
-                    />
-                  </Button.Group>
-                </Segment>
+                {this.renderBangumiSegment()}
+                {this.renderEpisodeSegment()}
+                {this.renderUploaderSegment()}
+                {this.renderTagsSegment()}
+                {this.renderActionSegment()}
               </Segment.Group>
             </Grid.Column>
 
             <Grid.Column width={12}>
-              <ZoomableImage
-                id={_id}
-                src={getImageUrl(file.medium)}
-                dataOriginal={getImageUrl(file.large)}
-                zooming={new Zooming()}
-              />
+              {this.renderImage()}
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </Container>
+    )
+  }
+
+  renderBangumiSegment = () => {
+    const { bangumis, screenshot } = this.props
+    if (!screenshot) return null
+    const bangumi = bangumis[screenshot.bangumi]
+    if (!bangumi) return null
+
+    return (
+      <Segment>
+        <Header>
+          <Link to={`/bangumi/${bangumi._id}`}>
+            {bangumi.title}
+          </Link>
+        </Header>
+
+        {bangumi.aliases.length > 0 &&
+          <List>
+            {bangumi.aliases.map(alias =>
+              <List.Item>{alias}</List.Item>
+            )}
+          </List>
+        }
+      </Segment>
+    )
+  }
+
+  renderEpisodeSegment = () => {
+    const { screenshot } = this.props
+    if (!screenshot) return null
+
+    return (
+      <Segment>
+        Episode {screenshot.episode}
+      </Segment>
+    )
+  }
+
+  renderUploaderSegment = () => {
+    const { users, screenshot } = this.props
+    if (!screenshot) return null
+
+    const uploader = users[screenshot.user]
+    if (!uploader) return null
+
+    return (
+      <Segment>
+        Uploader: <Link to={`/user/${uploader._id}`}>{uploader.username}</Link>
+      </Segment>
+    )
+  }
+
+  renderTagsSegment = () => {
+    const { screenshot } = this.props
+    if (!screenshot || screenshot.tags.length === 0) return null
+
+    return (
+      <Segment>
+        <Tags tags={screenshot.tags} />
+      </Segment>
+    )
+  }
+
+  renderActionSegment = () => {
+    const { dispatch, isAuthenticated, screenshot, screenshotFavorites, userFavorites } = this.props
+    if (!screenshot) return null
+    const { _id, file } = screenshot
+
+    const isFavorited = isAuthenticated && userFavorites &&
+      screenshotFavorites[_id].ids.filter(favoriteId => {
+        return userFavorites.ids.includes(favoriteId)
+      }).length > 0
+
+    const favoritesCount = screenshotFavorites[_id].ids.length
+
+    return (
+      <Segment>
+        <Button.Group>
+          <FavoriteButton
+            dispatch={dispatch}
+            screenshotId={_id}
+            isFavorited={isFavorited}
+            favoritesCount={favoritesCount}
+            isAuthenticated={isAuthenticated}
+          />
+          <DownloadButton
+            file={file}
+          />
+        </Button.Group>
+      </Segment>
+    )
+  }
+
+  renderImage = () => {
+    const { screenshot } = this.props
+    if (!screenshot) return null
+
+    const { _id, file } = screenshot
+
+    return (
+      <ZoomableImage
+        id={_id}
+        src={getImageUrl(file.medium)}
+        dataOriginal={getImageUrl(file.large)}
+        zooming={new Zooming()}
+      />
     )
   }
 }
@@ -116,18 +164,25 @@ class Screenshot extends Component {
 Screenshot.propTypes = {
   dispatch: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
+  screenshot: PropTypes.object,
+  users: PropTypes.object.isRequired,
+  bangumis: PropTypes.object.isRequired,
+  screenshotFavorites: PropTypes.object.isRequired,
+  userFavorites: PropTypes.object
 }
 
 function mapStateToProps(state, ownProps) {
   const { entities, user, screenshotFavorites, userFavorites } = state
   const { users, bangumis, screenshots } = entities
   const { isAuthenticated, uid } = user
+  const { screenshotId } = ownProps.params
+  const screenshot = screenshots[screenshotId]
 
   return {
     isAuthenticated,
+    screenshot,
     users,
     bangumis,
-    screenshots,
     screenshotFavorites,
     userFavorites: userFavorites[uid]
   }
