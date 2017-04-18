9f2b633c3443ae2f150e6a55d2347790f1d962cb
kingdido999 <kingdido999@gmail.com>
2017-04-18 15:49:32 -0700

    Make tags table unstackable

--- client/src/components/TagsPage.js
+++ client/src/components/TagsPage.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { Container, Segment, Table } from 'semantic-ui-react'
+import { Container, Table } from 'semantic-ui-react'
 import { Link } from 'react-router'
 
 import { getTags } from '../actions/entities'
@@ -8,42 +8,43 @@ const propTypes = {
   dispatch: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
   tagNames: PropTypes.array.isRequired,
-  tags: PropTypes.object.isRequired,
+  tags: PropTypes.object.isRequired
 }
 
 class TagsPage extends Component {
-
-  componentWillMount () {
+  componentWillMount() {
     const { dispatch } = this.props
     dispatch(getTags())
   }
 
-  render () {
+  render() {
     const { tags, tagNames } = this.props
 
     return (
       <Container text>
-        <Segment>
-          <Table celled striped>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Screenshots</Table.HeaderCell>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Type</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
+        <Table celled striped unstackable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Screenshots</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Type</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-            <Table.Body>
-              {tagNames.map((name, index) =>
-                <Table.Row key={index}>
-                  <Table.Cell collapsing>{tags[name].screenshots.length}</Table.Cell>
-                  <Table.Cell><Link to={`/tag/${name}`}>{name}</Link></Table.Cell>
-                  <Table.Cell>{tags[name].type}</Table.Cell>
-                </Table.Row>
-              )}
-            </Table.Body>
-          </Table>
-        </Segment>
+          <Table.Body>
+            {tagNames.map((name, index) => (
+              <Table.Row key={index}>
+                <Table.Cell collapsing>
+                  {tags[name].screenshots.length}
+                </Table.Cell>
+                <Table.Cell>
+                  <Link to={`/tag/${name}`}>{name}</Link>
+                </Table.Cell>
+                <Table.Cell>{tags[name].type}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
       </Container>
     )
   }
