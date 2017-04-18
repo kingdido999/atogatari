70dce200095ae05af15d5716fae770b6546de77e
kingdido999 <kingdido999@gmail.com>
2017-04-18 16:36:02 -0700

    Clean up

--- client/src/actions/common.js
+++ client/src/actions/common.js
@@ -1,5 +1,8 @@
 import { makeActionCreator } from '../utils'
 
+export const setErrorMessage = makeActionCreator('SET_ERROR_MESSAGE', 'message')
+export const resetErrorMessage = makeActionCreator('RESET_ERROR_MESSAGE')
+
 export function resetErrorMessageIfNeeded() {
 	return (dispatch, getState) => {
 		const { errorMessage } = getState()
@@ -10,5 +13,3 @@ export function resetErrorMessageIfNeeded() {
 	}
 }
 
-export const setErrorMessage = makeActionCreator('SET_ERROR_MESSAGE', 'message')
-export const resetErrorMessage = makeActionCreator('RESET_ERROR_MESSAGE')
