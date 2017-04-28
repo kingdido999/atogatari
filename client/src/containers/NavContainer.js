2bb23c3dbfd7339a342219573f45eb7ad7d49ceb
kingdido999 <kingdido999@gmail.com>
2017-04-28 13:31:51 -0700

    Clean up NavContainer

--- client/src/containers/NavContainer.js
+++ client/src/containers/NavContainer.js
@@ -10,27 +10,15 @@ class NavContainer extends Component {
 }
 
 function mapStateToProps(state) {
-  const { environment, user, search, routing } = state
+  const { environment, user, search } = state
   const { isMobile } = environment
   const { isAuthenticated, uid } = user
-  const { locationBeforeTransitions } = routing
-  const { pathname } = locationBeforeTransitions
-  const showFilters =
-    pathname === '/' ||
-    pathname.match('/screenshots') ||
-    pathname.match('/tag/') ||
-    pathname.match('/favorites') ||
-    pathname.match('/uploads')
-
-  const attached = showFilters ? 'top' : false
 
   return {
     isMobile,
     isAuthenticated,
     uid,
-    search,
-    showFilters,
-    attached
+    search
   }
 }
 
